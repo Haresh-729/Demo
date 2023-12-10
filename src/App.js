65409f46c199d1abb7efecb3818785f94716792b
Haresh Kurade <108918863+Haresh-729@users.noreply.github.com>
2023-12-11 01:00:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <button><a href='https://drive.google.com/file/d/1bCGcVe3ecl-J1jUwfJa7c9LaJx2Zf4_3/view?usp=drive_link'>Hello</a></button>
+      <button><a href='https://drive.google.com/file/d/1bCGcVe3ecl-J1jUwfJa7c9LaJx2Zf4_3/view'>Hello</a></button>
     </div>
   );
 }
